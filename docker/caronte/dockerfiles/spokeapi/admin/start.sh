34a04c0ec9b4f4536e158d9395e4c18b07ca8977
Mortviva7 <linascijunskisdodonova4@gmail.com>
2025-12-16 21:31:18 +0100

    cambio en el script

--- docker/caronte/dockerfiles/spokeapi/admin/start.sh
+++ docker/caronte/dockerfiles/spokeapi/admin/start.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 set -e
 
-# 1. Cargamos la configuración de Nginx (que a su vez carga la Base)
+# 1. Cargamos la configuración de Nginx
 echo "--- [POKEAPI] Cargando capas anteriores... ---"
-# Lo lanzamos en segundo plano (&) para que no se quede congelado ahí
 bash /root/admin/nginx/start.sh &  
 sleep 5
 
 # 2. Entramos en la carpeta del proyecto
-echo "--- [POKEAPI] Preparando React... ---"
+echo "--- [POKEAPI] Preparando React (Vite)... ---"
 cd /var/www/html/proyecto
 
-# 3. Instalamos dependencias si es la primera vez
+# 3. Instalamos dependencias
 if [ ! -d "node_modules" ]; then
-    echo "Instalando node_modules (esto puede tardar)..."
+    echo "Instalando node_modules..."
     npm install
 fi
 
-# 4. Arrancamos el servidor de desarrollo
-echo "--- [POKEAPI] Iniciando npm start ---"
-npm start
+# 4. Arrancamos Vite obligándole a usar el puerto 3000 y aceptar IPs externas
+echo "--- [POKEAPI] Iniciando Servidor Vite ---"
+npm run dev -- --host 0.0.0.0 --port 3000
